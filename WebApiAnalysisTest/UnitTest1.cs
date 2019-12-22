d04202603762c7933fb93c968a341c7461addb10
VK <vasilii@MacBook-Air-VK.local>
2019-12-23 01:31:55 +0300

    Added few base tests

--- WebApiAnalysisTest/UnitTest1.cs
+++ WebApiAnalysisTest/UnitTest1.cs
@@ -1,18 +1,58 @@
-using NUnit.Framework;
+using Xunit;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiAnalysis.Controllers;
+using Newtonsoft.Json;
+using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiAnalysis;
+using WebApiAnalysis.Interfaces;
+using WebApiAnalysis.Services;
+using WebApiAnalysis.Models;
 
 namespace WebApiAnalysisTest
 {
-    public class Tests
+    public class BaseControllerTest
     {
-        [SetUp]
-        public void Setup()
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public BaseControllerTest()
         {
+            _server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>()
+                .ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IDataStorage, DataStorage>();
+                    services.AddSingleton<WebApiAnalysis.Models.Settings>();
+                    services.AddControllers().AddNewtonsoftJson();
+                }));
+            _client = _server.CreateClient();
         }
 
-        [Test]
-        public void Test1()
+        [Fact]
+        public async void ServerRunningTest()
         {
-            Assert.Pass();
+            using (var response = await _client.GetAsync("/api/quizresults/test"))
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                Assert.Equal(responseString, "asd");
+            }
+        }
+
+        [Fact]
+        public async void RequestTest()
+        {
+            var requestJson = "";
+            var requestContent = new StringContent(requestJson, Encoding.Default, "application/json");
+
+            using (var response = await _client.PostAsync("/api/quizresults", requestContent)){
+                Assert.Equal(response.StatusCode, HttpStatusCode.BadRequest);
+            }
         }
     }
 }
